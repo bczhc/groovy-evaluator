3922ff7bbd2714cdeb8d7895813663d0b492e418
bczhc <bczhc0@126.com>
2022-01-08 19:08:14 +0800

    initial commit

---
+++ src/main/groovy/pers/zhc/Evaluator.groovy
@@ -0,0 +1,36 @@
+package pers.zhc
+
+class Evaluator {
+    static void main(String[] args) {
+        def sc = new Scanner(System.in)
+        def shell = new GroovyShell()
+        shell.setProperty("res", [])
+        def i = 0
+        // noinspection GroovyInfiniteLoopStatement
+        while (true) {
+            print "> "
+            System.out.flush()
+            def s = sc.nextLine()
+            try {
+                def result = shell.evaluate(s)
+                def resList = shell.getProperty("res")
+                if (resList instanceof List) {
+                    resList.add(result)
+                } else {
+                    shell.setProperty("res", [])
+                    i = 0
+                    (shell.getProperty("res") as List).add(i, result)
+                }
+
+                if (result == null) {
+                    println "res[$i] = $result"
+                } else {
+                    println "res[$i] = $result (${result.getClass()})"
+                }
+                ++i
+            } catch (e) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
